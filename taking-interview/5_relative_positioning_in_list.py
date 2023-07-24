cf86acfcc49af17f41c1efb6261375b3b6f96002
Leonard Topno <leo.topno@gmail.com>
2023-07-24 21:18:43 +0530

    Updated comments in relative_positioning_in_list

--- taking-interview/5_relative_positioning_in_list.py
+++ taking-interview/5_relative_positioning_in_list.py
@@ -1,28 +1,29 @@
 """
-Ques: Bring an item to the beginning of the list, keeping the relative position of rest if the items intact
+Ques: Given a list, bring an item to the beginning of the list,
+keeping the relative positions of the rest of the items intact
 """
 fruits_list = ['Apple', 'Banana', 'Coconut', 'Durian', 'Elderberry', 'Fig', 'Grape']
-print(fruits_list)
+print(f"Original fruits list: {fruits_list}")
 
 
 def bring_to_the_beginning_of_list():
 
-    item1 = input("Type the item to be brought to the beginning of the list: ")
+    item = input("Type the item to be brought to the beginning of the list: ")
 
-    # Get to know the index of the item in the list
-    index_1 = fruits_list.index(item1)
+    # i) Get to know the index of the item to be brought to the first
+    index_ = fruits_list.index(item)
 
-    # Pop that item out (you will have that item in hand, stored in a variable)
-    item_ = fruits_list.pop(index_1)  # pop takes index as argument and  
+    # ii) Pop that item out (you will have that item in hand, stored in a variable)
+    item_ = fruits_list.pop(index_)  # pop takes index as argument and returns the item pop-ed out
 
-    # Now insert that item_ in hand which you obtained by pop(index_of_item)
+    # iii) Now insert that item_ in hand which you obtained by pop(index_of_item)
     fruits_list.insert(0, item_)
 
     return fruits_list
 
 
 while True:
-    print(bring_to_the_beginning_of_list())
+    print("\n", bring_to_the_beginning_of_list(), "\n")
 
 
 # ------ One liner for the same function ---------
@@ -43,9 +44,12 @@ it does not alter the relative position of the rest of the elements.
     fruits_list.pop("Coconut") : Incorrect
     
 3) the output of .pop(index_) is the item at INDEX index_. It is can stored in a variable at used later.
+    Simultaneously list.pop(index_) has altered the list, as in it has removed the item at INDEX index_.
+    So list now contains one element less.
 
 4) use of list.index() func. 
     fruits_list.index("Coconut")
+5) Additional info on list and tuple: tuple has no .sort() inbuilt func, only list has.
  
 """
 
